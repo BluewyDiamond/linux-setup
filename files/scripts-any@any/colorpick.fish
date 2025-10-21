bc5277c9f18fddb5cce8c51af4961676d7cdd596
BluewyDiamond <bluewy.s.network@gmail.com>
2025-10-21 03:14:47 +0200

    save

--- files/scripts-any@any/colorpick.fish
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env fish
-
-set script_name (basename (status filename))
-
-set hyprpicker_output (hyprpicker; or notify-send "$script_name" "Failed, status code non zero!")
-wl-copy $hyprpicker_output
-notify-send "$script_name" "Copied $hyprpicker_output to clipboard."
--- files/scripts-any@any/pick_color.fish
+++
@@ -1,6 +0,0 @@
-#!/bin/env fish
-
-set script_name (basename (status filename))
-set picked_color (hyprpicker --autocopy)
-
-notify-send "$script_name" "<span color='$picked_color'> $picked_color 󰇘󰇘󰇘 clipboard</span>"
